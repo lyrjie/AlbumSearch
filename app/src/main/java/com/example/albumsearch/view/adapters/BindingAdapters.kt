69b54752e4dae3ec1599831f23f484a6cee88ef2
lyrjie <lyrjie@gmail.com>
2020-10-06 13:14:18 +0300

    Search: loading/error placeholders for images

--- app/src/main/java/com/example/albumsearch/view/adapters/BindingAdapters.kt
+++ app/src/main/java/com/example/albumsearch/view/adapters/BindingAdapters.kt
@@ -4,6 +4,8 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.albumsearch.R
 
 /**
  * Loads the image into [ImageView] using passed [url]
@@ -23,6 +25,11 @@ fun ImageView.loadImage(url: String?) {
         // Load
         Glide.with(this.context)
             .load(uri)
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
+            )
             .into(this)
     }
 }

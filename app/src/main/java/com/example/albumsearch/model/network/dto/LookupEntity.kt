69bfe75f7db31e86b82341a056a441a919a5f70b
lyrjie <lyrjie@gmail.com>
2020-10-06 17:48:21 +0300

    Added disclaimer

--- app/src/main/java/com/example/albumsearch/model/network/dto/LookupEntity.kt
+++ app/src/main/java/com/example/albumsearch/model/network/dto/LookupEntity.kt
@@ -22,3 +22,9 @@ data class LookupEntity(
     @Json(name = "wrapperType")
     val type: String
 )
+
+// DISCLAIMER: It being "LookupEntity" and not "Song" is related to ITunes for some reason also
+// returning album in a JSON array along with songs, even when you specifically ask for songs.
+// The proper way to do that would be to implement Song and Album as subclasses of Entity and write
+// your own Moshi mapper which decides which subclass to build on a per case basis. But it really
+// felt like an overkill for a small task.
